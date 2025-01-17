3dcf4915ba524862efcc01ae9a902c4583614092
Shivank Tripathi <182352677+devilshiv-07@users.noreply.github.com>
2025-01-17 14:41:55 +0530

    set up done

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
+const config = require('config');
+require('dotenv').config();
 
-mongoose.connect("mongodb://127.0.0.1:27017/scatch")
-.then( function(){
-    console.log("connected")
-})
-.catch( function(err){
-    console.log(err);
-})
+const dbgr = require('debug')('development:mongoose');
 
-module.exports = mongoose.connection; 
+mongoose
+   .connect(`${config.get("MONGODB_URI")}/scatch`)
+   .then( function(){
+       dbgr("connected")
+    })
+    .catch( function(err){
+       dbgr(err);
+    })
+
+module.exports = mongoose.connection;
--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", function (req, res) {
-    res.send("Hey");
+    res.send("Product route is working");
 })
 
 module.exports = router;
